6ca69cd8cf36a02518d05aea37877f696c21af86
Micah Levine <micah.levine@outlook.com>
2020-01-20 01:29:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Pig Latinizer!"
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @text = params[:user_phrase]
+    @piglatinizer = PigLatinizer.new
+    erb :piglatinize
+  end
+end
 end
